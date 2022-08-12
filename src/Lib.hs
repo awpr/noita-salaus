c0dbc91d550d275583169242946e43a8f4d56c6e
Andrew Pritchard <awpritchard@gmail.com>
2022-08-11 22:06:02 -0700

    Move Distribution and Histogram to a separate module.

--- src/Lib.hs
+++ src/Lib.hs
@@ -12,7 +12,6 @@ module Lib where
 
 import Control.Monad (replicateM)
 import Data.Char (chr, ord, isSpace)
-import Data.Coerce (coerce)
 import Data.Functor ((<&>))
 import Data.IntMap qualified as IM
 import Data.List (mapAccumL, transpose, tails)
@@ -34,39 +33,10 @@ import Data.RLE qualified as RLE
 import Data.Type.Attenuation (type (⊆))
 
 import Data
+import Salaus.Distribution
 import Salaus.Symbol
 
 _ = messages
-
-type Histogram a = KM.IntMap a (Sum Int)
-type Distribution a = KM.IntMap a Float
-
--- An IntMap containing the single mapping i => 1
-one :: a ⊆ Int => a -> Histogram a
-one i = KM.singleton i 1
-
-insertm :: (a ⊆ Int, Monoid m) => a -> m -> KM.IntMap a m -> KM.IntMap a m
-insertm k v m = KM.alter (Just . \case { Nothing -> v; Just x -> x <> v }) k m
-
-insertm' :: (Ord a, Monoid m) => a -> m -> M.Map a m -> M.Map a m
-insertm' k v m = M.alter (Just . \case { Nothing -> v; Just x -> x <> v }) k m
-
-histogram :: a ⊆ Int => [a] -> Histogram a
-histogram xs = coerce $ foldr (\x m -> insertm x (Sum @Int 1) m) KM.empty xs
-
-histogram' :: Ord a => [a] -> M.Map a Int
-histogram' xs = coerce $ foldr (\x m -> insertm' x (Sum @Int 1) m) M.empty xs
-
-toDistribution :: (Functor f, Foldable f) => f (Sum Int) -> f Float
-toDistribution h = fmap (\ (Sum x) -> fromIntegral x / n) h
- where
-  n = fromIntegral $ getSum $ sum h
-
-freqs :: a ⊆ Int => [a] -> Distribution a
-freqs = toDistribution . histogram
-
-rfreqs :: a ⊆ Int => Float -> [a] -> Distribution a
-rfreqs k xs = fmap (*k) $ freqs xs
 
 square :: Num a => a -> a
 square x = x * x
---
+++ src/Salaus/Distribution.hs
@@ -0,0 +1,45 @@
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE ImportQualifiedPost #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE MonoLocalBinds #-} -- shush a warning on simplifiable constraints
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE TypeOperators #-}
+
+module Salaus.Distribution where
+
+import Data.Coerce (coerce)
+import Data.Map.Strict qualified as M
+import Data.Semigroup (Sum(..))
+
+import Data.IntMap.Keyed qualified as KM
+import Data.Type.Attenuation (type (⊆))
+
+type Histogram a = KM.IntMap a (Sum Int)
+type Distribution a = KM.IntMap a Float
+
+-- An IntMap containing the single mapping i => 1
+one :: a ⊆ Int => a -> Histogram a
+one i = KM.singleton i 1
+
+insertm :: (a ⊆ Int, Monoid m) => a -> m -> KM.IntMap a m -> KM.IntMap a m
+insertm k v m = KM.alter (Just . \case { Nothing -> v; Just x -> x <> v }) k m
+
+insertm' :: (Ord a, Monoid m) => a -> m -> M.Map a m -> M.Map a m
+insertm' k v m = M.alter (Just . \case { Nothing -> v; Just x -> x <> v }) k m
+
+histogram :: a ⊆ Int => [a] -> Histogram a
+histogram xs = coerce $ foldr (\x m -> insertm x (Sum @Int 1) m) KM.empty xs
+
+histogram' :: Ord a => [a] -> M.Map a Int
+histogram' xs = coerce $ foldr (\x m -> insertm' x (Sum @Int 1) m) M.empty xs
+
+toDistribution :: (Functor f, Foldable f) => f (Sum Int) -> f Float
+toDistribution h = fmap (\ (Sum x) -> fromIntegral x / n) h
+ where
+  n = fromIntegral $ getSum $ sum h
+
+freqs :: a ⊆ Int => [a] -> Distribution a
+freqs = toDistribution . histogram
+
+rfreqs :: a ⊆ Int => Float -> [a] -> Distribution a
+rfreqs k xs = fmap (*k) $ freqs xs
