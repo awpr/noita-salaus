6ba407cca736d2e76178f9291378c188169d354c
Andrew Pritchard <awpritchard@gmail.com>
2022-08-11 22:06:32 -0700

    Add some plot utils based on `Chart`.

---
+++ src/Salaus/Plot.hs
@@ -0,0 +1,36 @@
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE ImportQualifiedPost #-}
+{-# LANGUAGE MonoLocalBinds #-} -- shush a warning on simplifiable constraints
+{-# LANGUAGE TypeOperators #-}
+
+module Salaus.Plot where
+
+import Data.Semigroup (getSum)
+import System.Process (callCommand)
+
+import Graphics.Rendering.Chart.Backend.Diagrams
+import Graphics.Rendering.Chart.Easy
+
+import Data.IntMap.Keyed qualified as KM
+import Data.Type.Attenuation (type (⊆), attenuate)
+
+import Salaus.Distribution
+
+viewPlot :: (Default r, ToRenderable r) => EC r () -> IO ()
+viewPlot ec = do
+  toFile (def & fo_size .~ (1920, 1080)) "/tmp/plot.svg" ec
+  callCommand "feh /tmp/plot.svg"
+
+plotHistograms :: a ⊆ Int => [String] -> [Histogram a] -> EC (Layout Int Int) ()
+plotHistograms nms h = do
+  layout_title .= "histogram"
+  p <- bars nms
+    [ (attenuate k :: Int, getSum <$> vs)
+    | (k, vs) <- KM.toList $ KM.unionsWith (++) $ map (fmap pure) h
+    ]
+  plot $ return $ plotBars (p & plot_bars_spacing .~ (BarsFixWidth 6))
+
+plotIoCSweep :: String -> String -> [(Int, Float)] -> EC (Layout Int Float) ()
+plotIoCSweep title param iocs = do
+  layout_title .= title
+  plot $ points param iocs
